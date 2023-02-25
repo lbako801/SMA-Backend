2f5d47a7710893a938b3c87d0428f06a5f323811
lbako801 <lbako98@gmail.com>
2023-02-24 21:19:04 -0700

    first iteration of mongoose models finished. Work required to reference all desired models to other models.

--- models/user.js
+++ models/user.js
@@ -1,51 +1,78 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
-  },
-  phone_number: {
-    type: String,
-    required: true
+    unique: true,
   },
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   unique_id: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: passwordValidator,
   },
   profile_pic: {
     type: String,
-    required: true
+    default: "/path/to/default/image.jpg",
   },
-  completed_prompts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Prompt'
-  }],
-  followers: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  following: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  liked_posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Prompt'
-  }]
+  prompts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Prompt",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  liked_posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
 });
 
-const User = mongoose.model('User', userSchema);
+const emailValidator = {
+  validator: function (value) {
+    const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(value);
+  },
+  message: "Please enter a valid email address",
+};
+
+const passwordValidator = {
+  validator: function (value) {
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(value);
+  },
+  message:
+    "Password must be at least 8 characters long and contain at least one capital letter, one number, and one special character.",
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
